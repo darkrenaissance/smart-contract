20bfd337d9c836f9d5cf2e1f774868038e296b86
parazyd <parazyd@dyne.org>
2023-02-25 17:13:04 +0100

    Nix flake

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ smart-contract
 ==============
 
 Template repository for building a DarkFi smart contract.
+
+## Deterministic builds
+
+We can use [Nix](https://nixos.wiki/) to deterministically build the
+WASM bincode. A `flake.nix` is provided, so just run `nix build`.
---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "Deterministic smart contract example";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ rust-overlay.overlay ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        rust = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        inputs = [ rust ];
+      in
+      {
+        defaultPackage = pkgs.rustPlatform.buildRustPackage {
+          pname = "smart-contract";
+          version = "0.0.1";
+
+          src = ./.;
+
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+
+          nativeBuildInputs = inputs;
+
+          buildPhase = ''
+            cargo build --release --target=wasm32-unknown-unknown
+          '';
+          installPhase = "mkdir -p $out; cp -f target/wasm32-unknown-unknown/release/smart_contract.wasm $out/";
+        };
+
+
+        devShell = pkgs.mkShell { packages = inputs; };
+      }
+    );
+}
